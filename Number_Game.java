5feeaea28ed7674e965da3ecfda201c078bd0741
Akshay00798 <106807367+Akshay00798@users.noreply.github.com>
2022-06-03 14:43:44 +0530

    Add files via upload

---
+++ Number_Game.java
@@ -0,0 +1,27 @@
+/**
+ * Gues the  number
+ */
+import java.util.Scanner;
+import java.util.Random;
+public class Number_Game {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Guess the Number Game !");
+
+        Random num = new Random();
+        int Computer_Move = num.nextInt(5 - 1) + 1;
+        
+        Scanner x = new Scanner(System.in);
+
+        int Human_Move;
+        System.out.println("Enter your Guess between 1 to 5");
+        Human_Move = x.nextInt();
+
+        if (Human_Move == Computer_Move) {
+            System.out.println("You win ");
+        } else {
+            System.out.println("Computer chose: " + Computer_Move);
+            System.out.println("You lose");
+        }
+    }
+}
